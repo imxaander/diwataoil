8750d00634922b2b679c8530b0b115983deb53bc
imxaander <xander22.ison@gmail.com>
2023-01-29 07:46:14 +0800

    fix script.js showdivs init

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ function openTab(evt, tabName) {
 var slideIndex = [1,1];
 var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
 showDivs(1, 0);
+showDivs(1, 1);
 showDivs(1, 2);
 showDivs(1, 3);
 showDivs(1, 4);
-showDivs(1, 5);
 
 function plusDivs(n, no) {
   showDivs(slideIndex[no] += n, no);
