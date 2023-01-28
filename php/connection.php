128978c519eeb06a1c8044069fa79d10ab3ba22d
imxaander <xander22.ison@gmail.com>
2023-01-29 07:26:59 +0800

    asd

--- php/connection.php
+++ php/connection.php
@@ -4,7 +4,13 @@ $user = "u324000355_amriennt";
 $password = "Amriennt2023.";
 $db_name = "u324000355_diwataoil";
 
-$con = new mysqli($host,$user,$password,$db_name);
+// Create connection
+$conn = mysqli_connect($host, $user, $password);
 
-
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
   ?>
